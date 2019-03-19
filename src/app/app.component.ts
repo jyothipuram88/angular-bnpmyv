4da521bff82748ed82489a1eeca0b477948e9388
jyothipuram88 <45223589+jyothipuram88@users.noreply.github.com>
2019-03-19 15:12:43 +0530

    v.0.1 kleeio changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'cm-app-component',
+  templateUrl: './app.component.html'
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { LoginModule } from './login/login.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    LoginModule,          // Eager loaded since we may need to go here right away as browser loads based on route user enters
+    AppRoutingModule,     // Main routes for application
+    CoreModule,           // Singleton objects (services, components that are loaded only once, etc.)
+    SharedModule          // Shared (multi-instance) objects
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
